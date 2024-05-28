90f9cbd183aebea60e0c7ada8e2b887c6367ab0d
Finn Groenewoud <finn.groenewoud@gmail.com>
2024-05-28 21:44:20 +0200

    Added initial Fabric project

---
+++ src/client/java/forcefields/ForceFieldsClient.java
@@ -0,0 +1,10 @@
+package forcefields;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ForceFieldsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
